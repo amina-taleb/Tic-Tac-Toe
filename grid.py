35a1bd5ac09129f81d89c03420b51a43790675da
morane-boughedada <morane.boughedada@laplateforme.io>
2024-12-03 12:37:28 +0100

    add grid_final

--- grid.py
+++ grid.py
@@ -1,13 +1,13 @@
- #Initialiser la grille vide
+
 grid = [[" " for _ in range(3)] for _ in range(3)]
 
-# Fonction pour afficher la grille
-def print_grid():
-    print("    A   B   C")
-    for i in range(3):
-        print(f" {i+1}| {grid[i][0]} | {grid[i][1]} | {grid[i][2]} |")
-        if i < 2:
-         print("  -------------")
 
-# Exemple d'utilisation
+def print_grid():
+    print("   A   B   C")
+    for i in range(3):
+        print(f" {i+1} {grid[i][0]} | {grid[i][1]} | {grid[i][2]} ")
+        if i < 2:
+         print("  ---+---+---")
+
+
 print_grid()
