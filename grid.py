1b21673ae22c4926a10249f0ac8c3210592a0934
morane-boughedada <morane.boughedada@laplateforme.io>
2024-12-03 10:42:05 +0100

    add newgrid with letter colum and number line

--- grid.py
+++ grid.py
@@ -3,10 +3,11 @@ grid = [[" " for _ in range(3)] for _ in range(3)]
 
 # Fonction pour afficher la grille
 def print_grid():
-    print("-------------")
+    print("    A   B   C")
     for i in range(3):
-        print(f"| {grid[i][0]} | {grid[i][1]} | {grid[i][2]} |")
-        print("-------------")
+        print(f" {i+1}| {grid[i][0]} | {grid[i][1]} | {grid[i][2]} |")
+        if i < 2:
+         print("  -------------")
 
 # Exemple d'utilisation
 print_grid()
